50c03ed1bdfceed0e2bc520ed36b47fea1f563a7
Dev K <krixdurex@gmail.com>
2026-07-11 17:28:01 +0530

    Update embed.mjs

--- Assets/Themes/Default/embed.mjs
+++ Assets/Themes/Default/embed.mjs
@@ -5,9 +5,9 @@ export default {
   wrongcolor: "#FF0000",
   footertext: "Powered by Durex",
   footericon:
-    "https://cdn.discordapp.com/emojis/994612386713575534.gif?size=32&quality=lossless",
+    "https://cdn.discordapp.com/attachments/1371493916511305839/1525451125330018415/Hello_Kitty_Gif.gif?ex=6a536e91&is=6a521d11&hm=2a95f23e35fa642df320915214066bca7400acdc31d1cf5e0f0e3f30c0a0a8ba&",
   authoricon:
-    "https://cdn.discordapp.com/emojis/994612386713575534.gif?size=32&quality=lossless",
+    "https://cdn.discordapp.com/attachments/1371493916511305839/1525450372934668459/3580618qyqm9ip4ie.gif?ex=6a536dde&is=6a521c5e&hm=1cf571ecb6d1d6100fffd2af010af5268bd228da0313fde4198e52d56295b0f9&",
   loadingicon:
-    "https://cdn.discordapp.com/emojis/994612386713575534.gif?size=32&quality=lossless",
+    "https://cdn.discordapp.com/attachments/1371493916511305839/1525449569779974214/788708-discordchristmas.gif?ex=6a536d1e&is=6a521b9e&hm=a391f05f37b1d676f3b7a327736281515b8cc8fdb764aeb9ef13900cda353f6a&",
 };
